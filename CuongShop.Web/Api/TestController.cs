9c2fcc3933932ee22a6789482d6f8899a86104cf
Qcuong103 <quoccuong100300@gmail.com>
2023-03-24 14:04:58 +0700

    Bài 11: Triển khai Web API để tạo Resfull API

---
+++ CuongShop.Web/Api/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CuongShop.Web.Areas.Areas.Controllers
+{
+    public class TestController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ CuongShop.Web/Areas/Areas/AreasAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CuongShop.Web.Areas.Areas
+{
+    public class AreasAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Areas";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Areas_default",
+                "Areas/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
